3c467cab5ccdc9dfa534bd75ef3700cbf2094fe4
ic <76838137+izcoser@users.noreply.github.com>
2021-05-13 10:23:07 -0300

    Add dotfiles

---
+++ bspwm/bspwm_colors.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bspc config focused_border_color "#5b5147"
+bspc config normal_border_color  "#3f3831"
---
+++ copy_dots.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ~/.bashrc .
+cp ~/.bash_profile .
+cp ~/.Xresources .
+cp -a ~/.config/bspwm .
+cp -a ~/.config/dunst .
+cp -a ~/.config/sxhkd .
+
