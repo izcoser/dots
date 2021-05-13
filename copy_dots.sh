82284975de2137004e80f97111c23bc5bae9bbd9
ic <76838137+izcoser@users.noreply.github.com>
2021-05-13 10:51:45 -0300

    Get list of installed packages
    Also now copies a lock script.

--- copy_dots.sh
+++ copy_dots.sh
@@ -6,4 +6,7 @@ cp ~/.Xresources .
 cp -a ~/.config/bspwm .
 cp -a ~/.config/dunst .
 cp -a ~/.config/sxhkd .
+install -D ~/.local/bin/lock .local/bin/lock
 
+pacman -Qe | cut -d' ' -f1 > pacman_packages
+python3 -m pip freeze | sed 's/=.*//' > pip_packages
